81b8e328358de3e62d1ebe211b35bcaafe0fcc34
noel0714 <gkrrydn_ji@naver.com>
2020-12-28 14:56:50 +0900

    .
    .

--- Pytorch/contiguous.py
+++ Pytorch/contiguous.py
@@ -19,19 +19,19 @@ def main():
     if torch.cuda.is_available():
         print("This is tested on GPU\n")
 
-        a = torch.zeros(10, 10, dtype=torch.int32).cuda(0)
-        print(f"This is a = \n{a}")
-        print(f"id(a) = {id(a)}\n")
+        b = torch.zeros(10, 10, dtype=torch.int32).cuda(0)
+        print(f"This is b = \n{b}")
+        print(f"id(b) = {id(b)}\n")
 
-        a_conti = torch.Tensor.contiguous(a[5:])
-        print(f"This is contiguous a = \n{a_conti}")
-        print(f"id(contiguous a) = {id(a_conti)}\n")
+        b_conti = torch.Tensor.contiguous(b[5:])
+        print(f"This is contiguous b = \n{b_conti}")
+        print(f"id(contiguous b) = {id(b_conti)}\n")
 
-        a_conti[:] = 999
-        print(f"This is contiguous a = \n{a_conti}")
-        print(f"id(contiguous a) = {id(a_conti)}\n")
-        print(f"This is a = \n{a}")
-        print(f"id(a) = {id(a)}\n")
+        b_conti[:] = 999
+        print(f"This is contiguous b = \n{b_conti}")
+        print(f"id(contiguous b) = {id(b_conti)}\n")
+        print(f"This is b = \n{b}")
+        print(f"id(b) = {id(b)}\n")
 
 
 
@@ -55,7 +55,7 @@ tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], dtype=torch.int32)
-id(a) = 2191134760696
+id(a) = 2037422431992
 
 This is contiguous a = 
 tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -63,7 +63,7 @@ tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], dtype=torch.int32)
-id(contiguous a) = 2191134642648
+id(contiguous a) = 2037422313944
 
 This is contiguous a = 
 tensor([[999, 999, 999, 999, 999, 999, 999, 999, 999, 999],
@@ -71,7 +71,7 @@ tensor([[999, 999, 999, 999, 999, 999, 999, 999, 999, 999],
         [999, 999, 999, 999, 999, 999, 999, 999, 999, 999],
         [999, 999, 999, 999, 999, 999, 999, 999, 999, 999],
         [999, 999, 999, 999, 999, 999, 999, 999, 999, 999]], dtype=torch.int32)
-id(contiguous a) = 2191134642648
+id(contiguous a) = 2037422313944
 
 This is a = 
 tensor([[  0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
@@ -84,11 +84,11 @@ tensor([[  0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
         [999, 999, 999, 999, 999, 999, 999, 999, 999, 999],
         [999, 999, 999, 999, 999, 999, 999, 999, 999, 999],
         [999, 999, 999, 999, 999, 999, 999, 999, 999, 999]], dtype=torch.int32)
-id(a) = 2191134760696
+id(a) = 2037422431992
 
 This is tested on GPU
 
-This is a = 
+This is b = 
 tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -99,26 +99,26 @@ tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], device='cuda:0', dtype=torch.int32)
-id(a) = 2191197333688
+id(b) = 2037485341976
 
-This is contiguous a = 
+This is contiguous b = 
 tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], device='cuda:0', dtype=torch.int32)
-id(contiguous a) = 2191134760696
+id(contiguous b) = 2037451163992
 
-This is contiguous a = 
+This is contiguous b = 
 tensor([[999, 999, 999, 999, 999, 999, 999, 999, 999, 999],
         [999, 999, 999, 999, 999, 999, 999, 999, 999, 999],
         [999, 999, 999, 999, 999, 999, 999, 999, 999, 999],
         [999, 999, 999, 999, 999, 999, 999, 999, 999, 999],
         [999, 999, 999, 999, 999, 999, 999, 999, 999, 999]], device='cuda:0',
        dtype=torch.int32)
-id(contiguous a) = 2191134760696
+id(contiguous b) = 2037451163992
 
-This is a = 
+This is b = 
 tensor([[  0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
         [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
         [  0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
@@ -130,5 +130,5 @@ tensor([[  0,   0,   0,   0,   0,   0,   0,   0,   0,   0],
         [999, 999, 999, 999, 999, 999, 999, 999, 999, 999],
         [999, 999, 999, 999, 999, 999, 999, 999, 999, 999]], device='cuda:0',
        dtype=torch.int32)
-id(a) = 2191197333688
+id(b) = 2037485341976
 '''
